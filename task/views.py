e222a1992d7cb4769b30f9dae5dd82f5bcce7868
jp-mena <jpmena@miuandes.cl>
2024-11-06 16:31:44 -0300

    db working

--- task/views.py
+++ task/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 def list_task(request):
-    return render(request, 'list_task.html')
+    tasks = Task.objects.all()  # Recupera todos los registros de Task
+    return render(request, 'list_task.html', {'tasks': tasks})
